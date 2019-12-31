3c8b420358defb302a24d8932e9874689927d55d
Gregor von Bergen <gregor@vbergen.ch>
2019-12-31 19:18:16 +0100

    Update spider for users created

--- instascrapy/spiders/iguser.py
+++ instascrapy/spiders/iguser.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
+import json
+import time
+
 import scrapy
+import boto3
+
+from instascrapy.db import DynDB
 
 
 class IguserSpider(scrapy.Spider):
     name = 'iguser'
-    allowed_domains = ['instgram.com']
-    start_urls = ['http://instgram.com/']
+
+    def start_requests(self):
+        db = DynDB(table='instalytics_dev', region_name='eu-central-1')
+        all_users = db.get_all_users('GSI1')
+        for user in all_users:
+            url = 'https://www.instagram.com/{}/'.format(user)
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        json_object = json.loads(response.xpath('//script[@type="text/javascript"]')\
+                                 .re('window._sharedData = (.+?);</script>')[0])
+        user = json_object['entry_data']['ProfilePage'][0]['graphql']['user']
+        retrieved_at_time = time.gmtime()
+
+        yield {
+            'pk': 'US#' + user['username'],
+            'sk': 'US#UPDA#V1' + time.strftime("%Y-%m-%dT%H:%M:%S", retrieved_at_time),
+            'id': user['id'],
+            'retrieved_at_time': retrieved_at_time,
+            'json': user
+        }

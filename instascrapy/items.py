0029ef14e512ce253d33ee9bd714f37cbf323063
Gregor von Bergen <gregor@vbergen.ch>
2020-01-01 12:43:26 +0100

    Added IGUser scrapy.Item

--- instascrapy/items.py
+++ instascrapy/items.py
@@ -8,6 +8,29 @@
 import scrapy
 
 
+class IGUser(scrapy.Item):
+    biography = scrapy.Field()
+    external_url = scrapy.Field()
+    external_url_linkshimmed = scrapy.Field()
+    edge_followed_by_count = scrapy.Field()
+    edge_follow_count = scrapy.Field()
+    full_name = scrapy.Field()
+    has_channel = scrapy.Field()
+    highlight_reel_count = scrapy.Field()
+    id = scrapy.Field()
+    is_business_account = scrapy.Field()
+    is_joined_recently = scrapy.Field()
+    business_category_name = scrapy.Field()
+    is_private = scrapy.Field()
+    is_verified = scrapy.Field()
+    profile_pic_url = scrapy.Field()
+    profile_pic_url_hd = scrapy.Field()
+    username = scrapy.Field()
+    connected_fb_page = scrapy.Field()
+    latest_posts = scrapy.Field()
+
+
+
 class InstascrapyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()

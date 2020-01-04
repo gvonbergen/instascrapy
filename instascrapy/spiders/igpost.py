5900aa44ce406ac0089f99619f50b4f15709b043
Gregor von Bergen <gregor@vbergen.ch>
2020-01-04 15:47:53 +0100

    ig post started

--- instascrapy/spiders/igpost.py
+++ instascrapy/spiders/igpost.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from instascrapy.db import DynDB
+from instascrapy.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
 
 class IgpostSpider(scrapy.Spider):
+
     name = 'igpost'
-    allowed_domains = ['instgram.com']
-    start_urls = ['http://instgram.com/']
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.db = DynDB(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, table='instalytics_dev', region_name='eu-central-1')
+
+
+    def start_requests(self):
+        all_posts = self.db.get_category_all('POST', 'GSI1')
+        for post in all_posts:
+            url = 'https://www.instagram.com/p/{}'.format(post)
+            yield scrapy.Request(url=url, callback=self.parse, errback=self.errback, dont_filter=True)
 
     def parse(self, response):
+        test = response
+        pass
+
+    def errback(self, failure):
         pass

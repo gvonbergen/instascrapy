29d82047685ad53eea246345c15e679bcb02c2fd
Gregor von Bergen <gregor@vbergen.ch>
2020-02-02 16:34:14 +0100

    staticmethod for parser created

--- instascrapy/spiders/iguser.py
+++ instascrapy/spiders/iguser.py
@@ -13,6 +13,22 @@ from instascrapy.spider import DynDBSpider
 class IguserSpider(DynDBSpider):
     name = 'iguser'
 
+    @staticmethod
+    def _parse_user(loader, data):
+        FIRST_LEVEL_ITEMS = ['biography', 'external_url', 'external_url_linkshimmed', 'full_name', 'has_channel',
+                             'highlight_reel_count', 'id', 'is_business_account', 'is_joined_recently',
+                             'business_category_name', 'is_private', 'is_verified', 'profile_pic_url',
+                             'profile_pic_url_hd', 'username', 'connected_fb_page']
+        for entry in FIRST_LEVEL_ITEMS:
+            loader.add_value(entry, data.get(entry))
+        loader.add_value('edge_followed_by_count', data['edge_followed_by']['count'])
+        loader.add_value('edge_follow_count', data['edge_follow']['count'])
+        loader.add_value('last_posts', [post['node']['shortcode'] for post in
+                                        data['edge_owner_to_timeline_media']['edges']])
+        loader.add_value('user_json', data)
+        loader.add_value('retrieved_at_time', int(time.time()))
+        return loader
+
     def start_requests(self):
         all_users = self.db.get_category_all('USER', 'GSI1')
         for user in all_users:
@@ -20,27 +36,12 @@ class IguserSpider(DynDBSpider):
             yield scrapy.Request(url=url, callback=self.parse, errback=self.errback, dont_filter=True)
 
     def parse(self, response):
-        def _parse_user(loader, data):
-            FIRST_LEVEL_ITEMS = ['biography', 'external_url', 'external_url_linkshimmed', 'full_name', 'has_channel',
-                                 'highlight_reel_count', 'id', 'is_business_account', 'is_joined_recently',
-                                 'business_category_name', 'is_private', 'is_verified', 'profile_pic_url',
-                                 'profile_pic_url_hd', 'username', 'connected_fb_page']
-            for entry in FIRST_LEVEL_ITEMS:
-                loader.add_value(entry, data.get(entry))
-            loader.add_value('edge_followed_by_count', data['edge_followed_by']['count'])
-            loader.add_value('edge_follow_count', data['edge_follow']['count'])
-            loader.add_value('last_posts', [post['node']['shortcode'] for post in
-                                             data['edge_owner_to_timeline_media']['edges']])
-            loader.add_value('user_json', data)
-            loader.add_value('retrieved_at_time', int(time.time()))
-            return loader
 
         ig_user_dict = ig_extract_shared_data(response=response, category='user')
-        ig_user = IGLoader(item=IGUser(), response=response)
-        ig_user = _parse_user(ig_user, ig_user_dict)
+        ig_user = IGLoader(item=IGUser())
+        ig_user = self._parse_user(loader=ig_user, data=ig_user_dict)
 
         yield ig_user.load_item()
-
 
     def errback(self, failure):
         self.logger.debug(repr(failure))

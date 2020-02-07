57c501b740c47d7fecafdfc1c2f0a50a50b7dd56
Gregor von Bergen <gregor@vbergen.ch>
2020-02-07 20:08:00 +0100

    proxy policy adapted

--- instascrapy/policy.py
+++ instascrapy/policy.py
@@ -3,18 +3,12 @@ from scrapy.exceptions import IgnoreRequest
 
 
 class InstagramBanDetection(BanDetectionPolicy):
-    NOT_BAN_STATUSES = {200, 301, 302, 404}
-    NOT_BAN_EXCEPTIONS = (IgnoreRequest,)
+    NOT_BAN_STATUSES = {200, 404}
 
-    # Todo: Change rotating proxy behavior if item could not be retrieved
     def response_is_ban(self, request, response):
         if response.status not in self.NOT_BAN_STATUSES:
             return True
-        if response.status == 200 and not len(response.body):
-            return True
-        if response.status == 200 and 'login' in response.url:
-            return True
-        if response.status == 302:
+        if 'login' in response.url:
             return True
         return False
 

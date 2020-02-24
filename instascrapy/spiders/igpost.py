683560225b23f4766e0fb68bc45144e89fac7434
Gregor von Bergen <gregor@vbergen.ch>
2020-02-24 19:50:14 +0100

    IGPost black formatted

--- instascrapy/spiders/igpost.py
+++ instascrapy/spiders/igpost.py
@@ -10,62 +10,63 @@ from instascrapy.spider import TxMongoSpider
 
 
 class IgpostSpider(TxMongoSpider):
-    name = 'igpost'
+    name = "igpost"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.prefix = 'PO#'
-        self.secondary_key = 'POST'
+        self.prefix = "PO#"
+        self.secondary_key = "POST"
 
     def start_requests(self):
         all_posts = self.crawling_scope()
 
         for post in all_posts:
-            url = 'https://www.instagram.com/p/{}/'.format(post)
-            yield scrapy.Request(url=url, callback=self.parse, errback=self.errback, dont_filter=True)
-
+            url = "https://www.instagram.com/p/{}/".format(post)
+            yield scrapy.Request(
+                url=url, callback=self.parse, errback=self.errback, dont_filter=True
+            )
 
     @staticmethod
     def _parse_post(loader, data):
         POST_ELEMENTS = [
-            'id',
-            'shortcode',
-            'dimensions',
-            'fact_check_overall_rating',
-            'fact_check_information',
-            'display_url',
-            'display_resources',
-            'accessibility_caption',
-            'is_video',
-            'tracking_token',
-            'edge_media_to_tagged_user',
-            'edge_media_to_caption',
-            'caption_is_edited',
-            'has_ranked_comments',
-            'edge_media_to_parent_comment',
-            'edge_media_to_hoisted_comment',
-            'edge_media_preview_comment',
-            'comments_disabled',
-            'taken_at_timestamp',
-            'edge_media_preview_like',
-            'edge_media_sponsor_user',
-            'location',
-            'location.id',
-            'owner.username',
-            'owner.id',
-            'is_ad',
-            'edge_web_media_to_related_media',
+            "id",
+            "shortcode",
+            "dimensions",
+            "fact_check_overall_rating",
+            "fact_check_information",
+            "display_url",
+            "display_resources",
+            "accessibility_caption",
+            "is_video",
+            "tracking_token",
+            "edge_media_to_tagged_user",
+            "edge_media_to_caption",
+            "caption_is_edited",
+            "has_ranked_comments",
+            "edge_media_to_parent_comment",
+            "edge_media_to_hoisted_comment",
+            "edge_media_preview_comment",
+            "comments_disabled",
+            "taken_at_timestamp",
+            "edge_media_preview_like",
+            "edge_media_sponsor_user",
+            "location",
+            "location.id",
+            "owner.username",
+            "owner.id",
+            "is_ad",
+            "edge_web_media_to_related_media",
         ]
 
         try:
             for entry in POST_ELEMENTS:
-                loader.add_value(entry.replace('.', '_'), deep_dict_get(data, entry))
+                loader.add_value(entry.replace(".", "_"), deep_dict_get(data, entry))
         except TypeError:
             pass
 
-        loader.add_value('post_json', data)
-        loader.add_value('retrieved_at_time', int(time.time()))
-        loader.add_value('image_urls', data.get('display_url'))
+        loader.add_value("post_json", data)
+        loader.add_value("retrieved_at_time", int(time.time()))
+        loader.add_value("image_urls", data.get("display_url"))
         return loader
 
     def parse(self, response):
@@ -82,5 +83,5 @@ class IgpostSpider(TxMongoSpider):
         if failure.check(HttpError):
             response = failure.value.response
             if response.status == 404:
-                post = response.url.split('/')[-2:-1][0]
+                post = response.url.split("/")[-2:-1][0]
                 self.set_entity_deleted(post)
